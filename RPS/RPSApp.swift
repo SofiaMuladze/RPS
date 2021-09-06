19c13d257f1c141cb3d3b4ecf19836a95f0ea0b4
Sofia <So.muladze@gmail.com>
2021-09-07 01:21:12 +0200

    Initial Commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Sofia Muladze on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
